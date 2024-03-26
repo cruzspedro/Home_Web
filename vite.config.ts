2f3c1ec8365a268673ce1bcb3886808ca53d3466
Pedro Henrique <pedro.hencruzs@gmail.com>
2024-03-26 12:14:17 -0300

    deploy-changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  // base: "/Home_Web/",
+  base: "/Home_Web/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
